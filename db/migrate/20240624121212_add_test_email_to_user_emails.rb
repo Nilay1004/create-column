d8585b5e9e6b9aba57015e4979f8c2916b5da680
Nilay1004 <nilaykujur@gmail.com>
2024-06-25 11:45:38 +0000

    error fix

--- db/migrate/20240624121212_add_test_email_to_user_emails.rb
+++ db/migrate/20240624121212_add_test_email_to_user_emails.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddChatEnabledToUserOptions < ActiveRecord::Migration[6.1]
+class AddTestEmailToUserEmails < ActiveRecord::Migration[6.1]
   def change
     add_column :user_emails, :test_email, :string
   end
